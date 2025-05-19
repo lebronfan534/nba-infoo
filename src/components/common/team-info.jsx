5da2b2f54cf8cd5c7cdfe1ebb88229f1a5b74f41
LebronFan <Evans@DESKTOP-RSFT9QT>
2025-05-19 16:39:28 +0000

    Add additional team card components and import statement to team-info component

--- src/components/common/team-info.jsx
+++ src/components/common/team-info.jsx
@@ -1,11 +1,15 @@
-
+import '../index.js'
 
 
 export default function TeamCards() {
     return (
         <div className="bg-black"> 
             <AtlanticCards />
-            
+            <CentralCards />
+            <NorthWestCards />
+            <PacificCards />
+            <SouthEastCards />
+            <SouthWestCards />
         </div>
     );
 }
