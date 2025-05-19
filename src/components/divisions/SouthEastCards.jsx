cf0f5a40fa5da5bc1c422d8e3bb6f34812ea2bd2
LebronFan <Evans@DESKTOP-RSFT9QT>
2025-05-19 16:29:11 +0000

    Replace placeholder component with AtlanticCards component displaying filtered team data and details

--- src/components/divisions/SouthEastCards.jsx
+++ src/components/divisions/SouthEastCards.jsx
@@ -1,1 +1,50 @@
-<p><Testttt></Testttt></p>
+import teamsData from '../../data/team-data';
+
+
+export default function AtlanticCards() {
+    return (
+    <>        
+        <h1 className="title"> Teams </h1>
+            <div className="">
+                {teamsData.filter(teamsData => teamsData.division === "Atlantic").map(teamsData =>  (
+                <div key={teamsData.id} 
+                className=
+                "" 
+                >
+                    <img src={teamsData.logo} className="w-32 h-32 "></img>
+                    <div style={{color: "#FFFFFF"}} className="font-mono">
+                        <a href={teamsData.website} className="team-title">{teamsData.name} ({teamsData.abbreviation})</a>
+                        <p className="team-conference">   Conference: {teamsData.conference}  </p>
+                        <p className="team-divison"> Divison: {teamsData.division}  </p>
+                        <p className="team-established">Established: {teamsData.established} </p>
+                        <p className="team-Championships">Championships: {teamsData.championships} </p>
+                        <a className="team-roster"href={teamsData.roster}>Roster</a>
+                    </div>
+                    <div className="team-colors">
+                        <div
+                        className="color-box-primary"
+                        style={{ backgroundColor: teamsData.colors.primary, width:30, minHeight:30,}}
+                        >
+                        </div>
+                        <div
+                        className="color-box-secondary"
+                        style={{ backgroundColor: teamsData.colors.secondary, width:30, minHeight:30,}}
+                        >
+                        </div>
+                        <div
+                        className="color-box-tertiary"
+                        style={{ backgroundColor: teamsData.colors.tertiary, width:30, minHeight:30,}}
+                        >
+                        </div>
+                        <div
+                        className="color-box-quaternary"
+                        style={{ backgroundColor: teamsData.colors.quaternary, width:30, minHeight:30, }}
+                        >
+                        </div>
+                    </div>
+                </div>
+                ))}
+            </div>
+    </>
+    )
+}
