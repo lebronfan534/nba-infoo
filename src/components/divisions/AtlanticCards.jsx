6dd0b929d40dbc20e8bfeae6d186631a9a2e8cbe
LebronFan <Evans@DESKTOP-RSFT9QT>
2025-05-20 06:37:07 +0000

    Fix missing className attribute for secondary color div in AtlanticCards component

--- src/components/divisions/AtlanticCards.jsx
+++ src/components/divisions/AtlanticCards.jsx
@@ -27,6 +27,7 @@ export default function AtlanticCards() {
                         style={{  backgroundColor: teamsData.colors.primary, width:30, minHeight:30, }}
                         </div>
                         <div> 
+                        </div>
                         <div>
                         className="rounded-full"
                         style={{ backgroundColor: teamsData.colors.secondary, width:30, minHeight:30,}}
