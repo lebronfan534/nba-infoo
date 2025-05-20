dcc94303a2a18c9be6759a91073edfc7720a0621
LebronFan <Evans@DESKTOP-RSFT9QT>
2025-05-20 06:34:01 +0000

    Modify hover transformation to use 3D translation and adjust scaling

--- src/components/divisions/AtlanticCards.jsx
+++ src/components/divisions/AtlanticCards.jsx
@@ -24,7 +24,7 @@ export default function AtlanticCards() {
                     <div className="flex gap-2 ">
                         <div
                         className="rounded-full 
-                        transform transition-all duration-300 ease-out hover:-translate-y-10 hover:translate-x-14 hover:scale-[2.2]"
+                        transform transition-all duration-300 ease-out hover:translate-3d-[14px,-10px,0px] hover:scale-125"
                         style={{  backgroundColor: teamsData.colors.primary, width:30, minHeight:30, }}
                         >
                         </div>
