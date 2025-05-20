b97af023026f1f84d11a724f828f21d0ecca6614
LebronFan <Evans@DESKTOP-RSFT9QT>
2025-05-20 06:25:18 +0000

    Remove unnecessary class name from the roster link element

--- src/components/divisions/AtlanticCards.jsx
+++ src/components/divisions/AtlanticCards.jsx
@@ -12,14 +12,14 @@ export default function AtlanticCards() {
                 "" 
                 >
                     
-                    <div style={{color: "#FFFFFF"}} className="font-mono">
+                    <div className="font-mono">
                         <img src={teamsData.logo} className="w-32 h-32 "></img>
                         <a href={teamsData.website} className="team-title">{teamsData.name} ({teamsData.abbreviation})</a>
                         <p className="team-conference">   Conference: {teamsData.conference}  </p>
                         <p className="team-divison"> Divison: {teamsData.division}  </p>
                         <p className="team-established">Established: {teamsData.established} </p>
                         <p className="team-Championships">Championships: {teamsData.championships} </p>
-                        <a className="team-roster"href={teamsData.roster}>Roster</a> 
+                        <a className=""href={teamsData.roster}>Roster</a> 
                     </div>
                     <div className="flex gap-2">
                         <div
