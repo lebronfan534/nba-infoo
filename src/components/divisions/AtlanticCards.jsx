46b5b71a24646ba48b8810ba5916dfad862317cf
LebronFan <Evans@DESKTOP-RSFT9QT>
2025-05-20 06:40:30 +0000

    Add centering alignment to the AtlanticCards component's div element

--- src/components/divisions/AtlanticCards.jsx
+++ src/components/divisions/AtlanticCards.jsx
@@ -28,7 +28,7 @@ export default function AtlanticCards() {
                         >
                         </div>
                         <div
-                        className ="absolute bottom-full" 
+                        className ="absolute bottom-full left-1/2 transform -translate" 
                         >
                             {teamsData.colors.primary} 
                         </div>
