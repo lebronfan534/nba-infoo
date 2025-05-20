4c8e378aa2004ce5d309d4d2e08ffb128efd8230
LebronFan <Evans@DESKTOP-RSFT9QT>
2025-05-20 06:40:47 +0000

    Add padding to the div displaying primary team colors in AtlanticCards component

--- src/components/divisions/AtlanticCards.jsx
+++ src/components/divisions/AtlanticCards.jsx
@@ -28,7 +28,7 @@ export default function AtlanticCards() {
                         >
                         </div>
                         <div
-                        className ="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2" 
+                        className ="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1" 
                         >
                             {teamsData.colors.primary} 
                         </div>
