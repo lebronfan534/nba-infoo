3207f9d315a6df8755cb9331083bef49f0f21643
LebronFan <Evans@DESKTOP-RSFT9QT>
2025-05-20 06:46:49 +0000

    Add padding to team card container in AtlanticCards component

--- src/components/divisions/AtlanticCards.jsx
+++ src/components/divisions/AtlanticCards.jsx
@@ -12,7 +12,7 @@ export default function AtlanticCards() {
                 "" 
                 >
                     
-                    <div className="font-mono font text-white ">
+                    <div className="font-mono font text-white py-4">
                         <img src={teamsData.logo} className="w-32 h-32 "></img>
                         <a href={teamsData.website} className="team-title">{teamsData.name} ({teamsData.abbreviation})</a>
                         <p className="team-conference">   Conference: {teamsData.conference}  </p>
