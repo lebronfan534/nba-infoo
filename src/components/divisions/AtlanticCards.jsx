4cbf7c6584a0852cd2f730da6d139ab1da4680ec
LebronFan <Evans@DESKTOP-RSFT9QT>
2025-05-20 06:30:50 +0000

    Fix hover effect class to use correct scale transformation

--- src/components/divisions/AtlanticCards.jsx
+++ src/components/divisions/AtlanticCards.jsx
@@ -26,7 +26,7 @@ export default function AtlanticCards() {
                         className="rounded-full 
                         transition-transform hover:-translate-y-10 
                         transition-transform hover:translate-x-14 
-                        transition-scale hover: "
+                        transition-transform hover:scale- "
                         style={{  backgroundColor: teamsData.colors.primary, width:30, minHeight:30, }}
                         >
                         </div>
